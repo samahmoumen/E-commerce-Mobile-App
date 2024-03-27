50dbcb4e985010999ecfa5a9be61ea47d44a6033
amine <kendimohammedamine@gmail.com>
2024-03-27 16:31:38 +0000

    First commit1

---
+++ app/src/main/java/com/example/e_commerce_app/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerce_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.e_commerce_app.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
